e586fabbc91aff49b3863855de985da9c61378dc
Kateryna Laina <boba4ok@gmail.com>
2023-10-14 15:42:48 +0300

    hotfix: replace webp by svg, add animation

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,65 +1,137 @@
-import React, {useEffect, useRef} from 'react';
-import HeroImage from "./HeroImage";
+import React, {useEffect, useState} from 'react';
 import {useScroll} from "../../utils/useScroll";
-import {useMedia} from "../../hooks/useMedia";
-import {cloudsLg, cloudsSm, imagesLg, imagesSm} from "../../consts/ImagesHero";
+import styles from './Hero.module.css'
+import {ReactComponent as Hero9} from "../../assets/hero/hero-9.svg";
+import {ReactComponent as Hero8} from "../../assets/hero/hero-8.svg";
+import {ReactComponent as Hero7} from "../../assets/hero/hero-07.svg";
+import {ReactComponent as Hero6} from "../../assets/hero/hero-06.svg";
+import {ReactComponent as Hero5} from "../../assets/hero/hero-05.svg";
+import {ReactComponent as Hero4} from "../../assets/hero/hero-04.svg";
+import {ReactComponent as Hero3} from "../../assets/hero/hero-03.svg";
+import {ReactComponent as Hero2} from "../../assets/hero/hero-02.svg";
+import {ReactComponent as Hero1} from "../../assets/hero/hero-01.svg";
+import {ReactComponent as Train} from "../../assets/hero/train.svg";
+import {ReactComponent as Sun} from "../../assets/hero/sunflower.svg";
+import {ReactComponent as Bridge} from "../../assets/hero/bridge.svg";
+import {ReactComponent as Cloud1} from "../../assets/clouds/cloud1.svg";
+import {ReactComponent as Cloud2} from "../../assets/clouds/cloud2.svg";
+import {ReactComponent as Cloud3} from "../../assets/clouds/cloud3.svg";
+import {ReactComponent as Cloud4} from "../../assets/clouds/cloud4.svg";
+import {ReactComponent as Cloud5} from "../../assets/clouds/cloud5.svg";
+import {ReactComponent as Cloud6} from "../../assets/clouds/cloud6.svg";
+import {ReactComponent as Cloud7} from "../../assets/clouds/cloud7.svg";
+import {ReactComponent as Cloud8} from "../../assets/clouds/cloud8.svg";
+
+
 const Hero = () => {
   const scrollY = useScroll()
-  const {isMobile} = useMedia()
-
-  let images;
-  let clouds;
-  if(isMobile) {
-    images = imagesSm
-    clouds = cloudsSm
-  } else{
-    images = imagesLg
-    clouds = cloudsLg
-  }
-
-  const refs = useRef([]);
+  const [translateX, setTranslateX] = useState(0);
+  const [direction, setDirection] = useState(1); // 1 to the right, -1 to the left
 
   useEffect(() => {
-    const translateValues = [0, 0, 0, 0, 0, 0, 0, 0];
-    const moveElements = () => {
-      refs.current.forEach((element, index) => {
-        if (element) {
-          if(translateValues[index] <= 10) {
-            translateValues[index] -= 0.01;
-          } else if (translateValues[index] >= 0) {
-            translateValues[index] += 0.01;
-          }
-          element.style.transform = `translate3d(${translateValues[index]}%, 0, 0)`;
+    const moveClouds = () => {
+      setTranslateX(prev => {
+        let newDirection = direction;
+        if (prev >= 100 || prev <= -100) {
+          newDirection = -direction;
+          setDirection(newDirection);
         }
+        return prev + (0.5 * newDirection);
       });
     };
 
-    const intervalId = setInterval(moveElements, 50);
+    const intervalId = setInterval(moveClouds, 50);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [direction]);
+
+  const calculateTranslate = (scrollY, factor) => {
+    return `translate3d(0, ${scrollY * factor}px, 0)`;
+  };
 
   return (
-    <div className="section_hero">
-      <div className="hero-wrapper">
-        {images.map((item, index) =>
-          <HeroImage
-            src={item}
-            alt={`hero-${index + 1}`}
-            className={`hero-${index + 1}`}
-            scrollY={scrollY}
-          />
-        )}
-        {clouds.map((item, index) =>
-          <HeroImage
-            src={item}
-            alt={`cloud-${index + 1}`}
-            className={`hero-cloud${index + 1}`}
-            ref={(el) => (refs.current[index] = el)}
-          />
-        )}
+    <div className={styles.section_hero}>
+      <div className={styles['hero-wrapper']}>
+        <Hero9 width="100%" height="100%" className={styles['hero-1']}/>
+        <Hero8 width="100%" height="100%" className={styles['hero-2']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.1)
+               }}/>
+        <Hero7 width="100%" height="100%" className={styles['hero-3']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.2)
+               }}/>
+        <Hero6 width="100%" height="100%" className={styles['hero-4']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.4)
+               }}/>
+        <Hero5 width="100%" height="100%" className={styles['hero-5']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.4)
+               }}/>
+        <Hero4 width="100%" height="100%" className={styles['hero-6']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.1)
+               }}/>
+        <Hero3 width="100%" height="100%" className={styles['hero-7']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.4)
+               }}/>
+        <Hero2 width="100%" height="100%" className={styles['hero-8']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.4)
+               }}/>
+        <Hero1 width="100%" height="100%" className={styles['hero-9']}
+               style={{
+                 transform: calculateTranslate(scrollY, 0.4)
+               }}
+               />
+        <Bridge width="100%" height="100%" className={styles['hero-10']}
+        />
+        <Train className={styles['hero-11']} width="100%" height="100%"/>
+        <Sun className={styles['hero-12']} width="100%" height="100%"
+             style={{
+               opacity: scrollY/500,
+               transform: calculateTranslate(scrollY, 0.4)
+             }}/>
+        <Cloud1 width="100%" height="100%" className={styles['hero-cloud1']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud2 width="100%" height="100%" className={styles['hero-cloud2']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud3 width="100%" height="100%" className={styles['hero-cloud3']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud4 width="100%" height="100%" className={styles['hero-cloud4']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud5 width="100%" height="100%" className={styles['hero-cloud5']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud6 width="100%" height="100%" className={styles['hero-cloud6']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud7 width="100%" height="100%" className={styles['hero-cloud7']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+        <Cloud8 width="100%" height="100%" className={styles['hero-cloud8']}
+                style={{
+                  transform: `translate3d(${translateX}px, 0, 0)`
+                }}/>
+
       </div>
+
     </div>
-)
+
+
+  )
 };
 
 
--- src/consts/ImagesHero.js
+++ src/consts/ImagesHero.js
@@ -1,49 +1,22 @@
-import Hero11 from "../assets/hero/hero-11.webp";
-import Hero10 from "../assets/hero/hero-10.webp";
-import Hero9 from "../assets/hero/hero-09.webp";
-import Hero8 from "../assets/hero/hero-08.webp";
-import Hero7 from "../assets/hero/hero-07.webp";
-import Hero6 from "../assets/hero/hero-06.webp";
-import Hero5 from "../assets/hero/hero-05.webp";
-import Hero4 from "../assets/hero/hero-04.webp";
-import Hero3 from "../assets/hero/hero-03.webp";
-import Hero2 from "../assets/hero/hero-02.webp";
-import Hero1 from "../assets/hero/hero-01.webp";
-import Hero11Sm from '../assets/hero/hero-11-p-500.webp';
-import Hero10Sm from '../assets/hero/hero-10-p-500.webp';
-import Hero9Sm from '../assets/hero/hero-09-p-500.webp';
-import Hero8Sm from '../assets/hero/hero-08-p-500.webp';
-import Hero7Sm from '../assets/hero/hero-07-p-500.webp';
-import Hero6Sm from '../assets/hero/hero-06-p-500.webp';
-import Hero5Sm from '../assets/hero/hero-05-p-500.webp';
-import Hero4Sm from '../assets/hero/hero-04-p-500.webp';
-import Hero3Sm from '../assets/hero/hero-03-p-500.webp';
-import Hero2Sm from '../assets/hero/hero-02-p-500.webp';
-import Hero1Sm from '../assets/hero/hero-01-p-500.webp';
-import Cloud8 from "../assets/clouds/cloud8.webp";
-import Cloud7 from "../assets/clouds/cloud7.webp";
-import Cloud6 from "../assets/clouds/cloud6.webp";
-import Cloud5 from "../assets/clouds/cloud5.webp";
-import Cloud4 from "../assets/clouds/cloud4.webp";
-import Cloud3 from "../assets/clouds/cloud3.webp";
-import Cloud2 from "../assets/clouds/cloud2.webp";
-import Cloud1 from "../assets/clouds/cloud1.webp";
-import Cloud8Sm from "../assets/clouds/hero-cloud8-p-500.webp";
-import Cloud7Sm from "../assets/clouds/hero-cloud7-p-500.webp";
-import Cloud6Sm from "../assets/clouds/hero-cloud6-p-500.webp";
-import Cloud5Sm from "../assets/clouds/hero-cloud5-p-500.webp";
-import Cloud4Sm from "../assets/clouds/hero-cloud4-p-500.webp";
-import Cloud3Sm from "../assets/clouds/hero-cloud3-p-500.webp";
-import Cloud2Sm from "../assets/clouds/hero-cloud2-p-500.webp";
-import Cloud1Sm from "../assets/clouds/hero-cloud1-p-500.webp";
+import Hero11 from "../assets/hero/hero-9.svg";
+import Hero10 from "../assets/hero/hero-8.svg";
+import Hero9 from "../assets/hero/hero-07.svg";
+import Hero8 from "../assets/hero/hero-06.svg";
+import Hero7 from "../assets/hero/hero-05.svg";
+import Hero5 from "../assets/hero/bridge.svg";
+import Hero4 from "../assets/hero/hero-03.svg";
+import Hero3 from "../assets/hero/hero-02.svg";
+import Hero2 from "../assets/hero/hero-01.svg";
+import Cloud8 from "../assets/clouds/cloud8.svg";
+import Cloud7 from "../assets/clouds/cloud7.svg";
+import Cloud6 from "../assets/clouds/cloud6.svg";
+import Cloud5 from "../assets/clouds/cloud5.svg";
+import Cloud4 from "../assets/clouds/cloud4.svg";
+import Cloud3 from "../assets/clouds/cloud3.svg";
+import Cloud2 from "../assets/clouds/cloud2.svg";
+import Cloud1 from "../assets/clouds/cloud1.svg";
 
 
+export const images = [Hero11, Hero10, Hero9,Hero8, Hero7, Hero5,Hero4,Hero3,Hero2]
 
-
-export const imagesLg = [Hero11, Hero10, Hero9,Hero8, Hero7, Hero6, Hero5,Hero4,Hero3,Hero2,Hero1]
-
-export const imagesSm = [Hero11Sm, Hero10Sm, Hero9Sm,Hero8Sm, Hero7Sm, Hero6Sm, Hero5Sm,Hero4Sm,Hero3Sm,Hero2Sm,Hero1Sm]
-
-
-export const cloudsLg = [Cloud8, Cloud7, Cloud6,Cloud5, Cloud4, Cloud3,Cloud2,Cloud1]
-export const cloudsSm = [Cloud8Sm, Cloud7Sm, Cloud6Sm,Cloud5Sm, Cloud4Sm, Cloud3Sm,Cloud2Sm,Cloud1Sm]
+export const clouds = [Cloud8, Cloud7, Cloud6,Cloud5, Cloud4, Cloud3,Cloud2,Cloud1]
