012015893ea363d725a4c9437109abdb2a9041e1
Kateryna Laina <boba4ok@gmail.com>
2023-10-17 10:53:29 +0300

    feature: create client section

---
+++ src/Components/Clients/Clients.js
@@ -0,0 +1,33 @@
+import React, {useRef} from 'react';
+import styles from './Clients.module.css'
+import {ReactComponent as BubbleRight} from "../../assets/clients/bubbles_right.svg";
+import {ReactComponent as BubbleLeft} from "../../assets/clients/bubble_left.svg";
+import {ReactComponent as BubbleMiddle} from "../../assets/clients/bubbles_middle.svg";
+import {ReactComponent as Footer1} from "../../assets/clients/clients_footer.svg";
+import {ReactComponent as Footer2} from "../../assets/clients/clients_rock.svg";
+import {ReactComponent as Footer3} from "../../assets/clients/clients_rocks_2.svg";
+import {ReactComponent as Ship} from "../../assets/clients/ship.svg";
+import {ReactComponent as Mask} from "../../assets/clients/mask.svg";
+import Fish from "./Fish";
+
+const Clients = () => {
+  const sectionRef = useRef(null);
+  return (
+    <section className={styles.clients}>
+      <div ref={sectionRef} className={styles.wrapper}>
+        <BubbleRight width="100%" height="100%" className={styles['clients-01']} loading="eager"/>
+        <BubbleMiddle width="100%" height="100%" className={styles['clients-02']} loading="eager"/>
+        <BubbleLeft width="100%" height="100%" className={styles['clients-03']} loading="eager"/>
+        <Footer1 width="100%" height="100%" className={styles['clients-04']} loading="eager"/>
+        <Footer2 width="100%" height="100%" className={styles['clients-05']} loading="eager"/>
+        <Footer3 width="100%" height="100%" className={styles['clients-06']} loading="eager"/>
+        <Fish sectionRef={sectionRef}/>
+        <Ship width="100%" height="100%" className={styles['clients-07']} loading="eager"/>
+        <Mask width="100%" height="100%" className={styles['masque_bottom']} loading="eager"/>
+      </div>
+
+    </section>
+  );
+};
+
+export default Clients;
---
+++ src/Components/Clients/Fish.js
@@ -0,0 +1,103 @@
+import React, {useEffect, useState} from 'react';
+import styles from "./Clients.module.css";
+import {ReactComponent as Fish1} from "../../assets/clients/fish_1.svg";
+import {ReactComponent as Fish2} from "../../assets/clients/fish_2.svg";
+import {ReactComponent as Fish3} from "../../assets/clients/fish_3.svg";
+import {ReactComponent as Fish4} from "../../assets/clients/fish_4.svg";
+
+const Fish = ({sectionRef}) => {
+
+
+  const [fish1State, setFish1State] = useState({
+    x: Math.random() * 3,
+    y: 100,
+    z: 50,
+    xVelocity: Math.random() * 2,
+    yVelocity: Math.random() * 2,
+    zVelocity: Math.random() * 0.5,
+    direction: 'left'
+  });
+
+  const [fish2State, setFish2State] = useState({
+    x: Math.random() * 3,
+    y: 20,
+    z: 0,
+    xVelocity: Math.random() * 2,
+    yVelocity: Math.random() * 2,
+    zVelocity: Math.random() * 0.5,
+    direction: 'left'
+  });
+  const [fish3State, setFish3State] = useState({
+    x: Math.random() * 3,
+    y: 10,
+    z: 10,
+    xVelocity: Math.random() * 2,
+    yVelocity: Math.random() * 2,
+    zVelocity: Math.random() * 0.5,
+    direction: 'right'
+  });
+  const [fish4State, setFish4State] = useState({
+    x: Math.random() * 10,
+    y: 50,
+    z: 10,
+    xVelocity: Math.random() * 2,
+    yVelocity: Math.random() * 2,
+    zVelocity: Math.random() * 0.5,
+    direction: 'left'
+  });
+
+  const tick = (currentState, setStateFunc) => {
+    if (sectionRef.current) {
+      const maxWidth = sectionRef.current ? sectionRef.current.offsetWidth : 0;
+      const maxHeight = sectionRef.current ? sectionRef.current.offsetHeight : 0;
+      const maxDepth = 200;
+
+      let {x, y, z, xVelocity, yVelocity, zVelocity} = currentState;
+
+      x += xVelocity;
+      y += yVelocity;
+      z += zVelocity;
+      let direction = currentState.direction;
+      if (x < 0 || x > maxWidth) {
+        xVelocity = -xVelocity
+        direction = direction === 'left' ? 'right' : 'left';
+      }
+      if (y < 0 || y > maxHeight) yVelocity = -yVelocity;
+      if (z < 0 || z > maxDepth) zVelocity = -zVelocity;
+
+      setStateFunc({x, y, z, xVelocity, yVelocity, zVelocity, direction});
+    }
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(() => {
+      tick(fish1State, setFish1State);
+      tick(fish2State, setFish2State);
+      tick(fish3State, setFish3State);
+      tick(fish4State, setFish4State);
+    }, 50);
+
+    return () => {
+      clearInterval(timerID);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fish1State, fish2State, fish3State, fish4State, sectionRef]);
+
+  const getFishStyle = (fishState) => ({
+    position: 'absolute',
+    left: fishState.x,
+    top: fishState.y,
+    transform: `scaleX(${fishState.direction === 'right' ? 1 : -1}) scale(${1 + fishState.z / 200})`
+  });
+
+  return (
+    <>
+      <Fish1 style={getFishStyle(fish1State)} width="100%" height="100%" className={styles['fish-01']} loading="eager"/>
+      <Fish2 style={getFishStyle(fish2State)} width="100%" height="100%" className={styles['fish-02']} loading="eager"/>
+      <Fish3 style={getFishStyle(fish3State)} width="100%" height="100%" className={styles['fish-03']} loading="eager"/>
+      <Fish4 style={getFishStyle(fish4State)} width="100%" height="100%" className={styles['fish-04']} loading="eager"/>
+    </>
+  );
+};
+
+export default Fish;
